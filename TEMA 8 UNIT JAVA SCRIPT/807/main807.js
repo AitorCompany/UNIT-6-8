ab9d0974876f51ef7761714c8e986344dfeeff90
AitorCompany <fpbasicaaitor@gmail.com>
2022-03-16 08:43:06 +0100

    "ACTIVITAT 807"

---
+++ TEMA 8 UNIT JAVA SCRIPT/807/main807.js
@@ -0,0 +1,25 @@
+"use-strict";
+
+let books = [
+  {
+    title: 'The Design of EveryDay Things',
+    author: 'Don Norman',
+    alreadyRead: false,
+  },
+  {title: 'The Most Human Human',
+  author: 'Brian Christian',
+  alreadyRead: true,
+  }];
+
+for (let i = 0; i < books.length; i++) {
+  let book = books[i];
+  let bookInfo = book.title + '" by ' + book.author;
+  if (book.alreadyRead) {
+    console.log('You already read "' + bookInfo);
+  } else {
+    console.log('You still need to read "' + bookInfo);
+  }
+}
+document.addEventListener('DOMcontentLoaded, main');
+
+
